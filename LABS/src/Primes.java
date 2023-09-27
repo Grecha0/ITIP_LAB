b7d6d1225863b9af9c2dc186fada96d2cfad20bf
Grecha0 <tomaily.com@gmail.com>
2023-09-27 07:57:32 +0300

    first commit

---
+++ LABS/src/Primes.java
@@ -0,0 +1,20 @@
+public class Primes {
+    public static void main(String[] args) {
+        int n = 2;
+        while (n != 101) {
+            boolean a = isPrime(n);
+            if(a == (boolean)true) {
+                System.out.print(n + " ");
+            }
+            n++;
+        }
+
+    }
+    public static boolean isPrime(int n) {
+        for(int i = 2; i < n; i++) {
+            if(n % i == 0 && n!=i)
+                return false;
+        }
+        return true;
+    } 
+} 
